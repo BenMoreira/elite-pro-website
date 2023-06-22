81e1454cbd201ec58657ab5a4432b552d0cf9b45
BenMoreira <bmoreira@ggc.edu>
2023-06-22 14:47:04 -0400

    Adds to About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='flex flex-col justify-center items-center gap-12 mt-[3%]'>
+      <div className='text-black-100 font-bold text-2xl border-b-2 border-black-100'>
+        About Elite-Pro Construction Inc.
+      </div>
+
+      <div className='flex flex-row justify-between items-start gap-12'>
+        <div className='border-2 border-black-100 rounded-lg px-[12vw] py-[25vh]'>
+          About Info Here
+        </div>
+
+        <div className='border-2 border-black-100 rounded-lg px-[12vw] py-[20vh]'>
+          Image Here
+        </div>
+      </div>
+
+      <div>
+        <a href='/Contact' className='border-2 border-black-100 rounded-lg px-5 py-2 font-bold hover:text-red-100 hover:border-red-100'>
+          Contact Us
+        </a>
+      </div>
+    </div>
   )
 }
 
