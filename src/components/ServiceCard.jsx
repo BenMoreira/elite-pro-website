e0a62c59d7bb985dccd68783f472a8a6490bf000
BenMoreira <bmoreira@ggc.edu>
2023-06-22 15:42:31 -0400

    Adds ServiceCard component, and adds to Services

---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const ServiceCard = ({name, desc, image}) => {
+  return (
+    <div className='w-full'>
+        <div className='flex flex-row justify-between items-start gap-12'>
+            <div>
+                <img src={image} alt='image' className='w-[150px]' />
+            </div>
+
+            <div className='flex flex-col items-end gap-2 text-black-100'>
+                <div className='font-bold text-xl border-b border-black-100'>
+                    {name}
+                </div>
+
+                <div className=''>
+                    {desc}
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ServiceCard
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import ServiceCard from '../components/ServiceCard'
+import reactPic from '../assets/react.svg'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div className='mx-[20%]'>
+      <div className='flex flex-col justify-center items-start gap-12 mt-[3%]'>
+        <div className='text-black-100 font-bold text-2xl border-b-2 border-black-100'>
+          Services We Provide
+        </div>
+
+        <ServiceCard name='name' desc='description' image={reactPic} />
+        <ServiceCard name='name' desc='description' image={reactPic} />
+        <ServiceCard name='name' desc='description' image={reactPic} />
+        <ServiceCard name='name' desc='description' image={reactPic} />
+      </div>
+    </div>
   )
 }
 
